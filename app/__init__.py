879ea7ef5ee56bdf16616b986205be1d6866ac7e
lazyKEV <viveksinha.301@gmail.com>
2019-05-15 20:17:26 +0530

    updated disease model and resource

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,8 @@ import os   # noqa
 
 from app.models import db   # noqa
 from app.resources import oauth     # noqa
-from app.resources.disease import Disease, DiseaseList, RegisterDiseases    # noqa
+from app.resources.disease import (Disease, DiseaseList,  DiseaseListOfPlant,   # noqa
+                                   RegisterDiseases, UpdateDisease)
 from app.resources.google_login import GoogleLogin, GoogleAuthorize     # noqa
 from app.resources.image import ImageList, ImageUpload      # noqa
 from app.resources.plant import Plant, PlantList, RegisterPlants    # noqa
@@ -42,9 +43,11 @@ def create_app():
     api.add_resource(Plant, '/api/plant/<string:name>')
     api.add_resource(PlantList, '/api/plants')
     api.add_resource(RegisterPlants, '/api/register/plants')
-    api.add_resource(Disease, '/api/disease/<string:name>/<int:plant_id>')
+    api.add_resource(Disease, '/api/disease/<int:disease_id>')
     api.add_resource(DiseaseList, '/api/diseases')
+    api.add_resource(DiseaseListOfPlant, '/api/diseases/<int:plant_id>')
     api.add_resource(RegisterDiseases, '/api/register/diseases')
+    api.add_resource(UpdateDisease, '/api/update/disease/<int:disease_id>')
     api.add_resource(ImageUpload, "/api/upload/image/<string:plant_name>")
     api.add_resource(ImageList, "/api/images")
     api.add_resource(GoogleLogin, "/login/google")
