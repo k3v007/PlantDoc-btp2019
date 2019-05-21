48572d5b23ea9862f86d9f20a7a84fc3b31ae6eb
lazyKEV <viveksinha.301@gmail.com>
2019-05-22 03:07:04 +0530

    gunicorn and logs

---
+++ gunicorn.sh
@@ -0,0 +1,8 @@
+gunicorn run:app\
+    --workers 4\
+    --bind 127.0.0.1:5000\
+    --timeout 120\
+    --log-level INFO\
+    --access-logfile logs/gunicorn.access.log\
+    --error-logfile logs/gunicorn.error.log\
+    --reload
